14e43cefc93d775b91df41dc06f663f56da62ea3
igrisdev <johanalvarezz237@gmail.com>
2026-04-23 16:05:31 -0500

    feat: add the Entity and Builder

--- src/main/java/com/igrisdev/PruebaTecSupermercado/model/DetalleVenta.java
+++ src/main/java/com/igrisdev/PruebaTecSupermercado/model/DetalleVenta.java
@@ -1,9 +1,6 @@
 package com.igrisdev.PruebaTecSupermercado.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +10,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class DetalleVenta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
