6422a2d298919e2bbacd1ab7f0b6ccd9dbef5bfd
igrisdev <johanalvarezz237@gmail.com>
2026-04-23 16:11:03 -0500

    feat: add a new metod in the Repository with Jpa

--- src/main/java/com/igrisdev/PruebaTecSupermercado/repository/ProductoRepository.java
+++ src/main/java/com/igrisdev/PruebaTecSupermercado/repository/ProductoRepository.java
@@ -3,5 +3,10 @@ package com.igrisdev.PruebaTecSupermercado.repository;
 import com.igrisdev.PruebaTecSupermercado.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    // buscar producto por nombre
+    Optional<Producto> findByNombre(String nombre);
 }
