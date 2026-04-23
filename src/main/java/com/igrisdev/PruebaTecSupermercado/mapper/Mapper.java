fc8ffbf0ae750b48b1c567b1a28f65d96efc12bf
igrisdev <johanalvarezz237@gmail.com>
2026-04-23 16:09:32 -0500

    feat: created the mappers from models

---
+++ src/main/java/com/igrisdev/PruebaTecSupermercado/mapper/Mapper.java
@@ -0,0 +1,68 @@
+package com.igrisdev.PruebaTecSupermercado.mapper;
+
+
+import com.igrisdev.PruebaTecSupermercado.dto.DetalleVentaDTO;
+import com.igrisdev.PruebaTecSupermercado.dto.ProductoDTO;
+import com.igrisdev.PruebaTecSupermercado.dto.SucursalDTO;
+import com.igrisdev.PruebaTecSupermercado.dto.VentaDTO;
+import com.igrisdev.PruebaTecSupermercado.model.Producto;
+import com.igrisdev.PruebaTecSupermercado.model.Sucursal;
+import com.igrisdev.PruebaTecSupermercado.model.Venta;
+
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Mapper {
+
+    // Mapeo de producto a ProductoDTO
+    public static ProductoDTO toDTO(Producto p) {
+        if (p == null) return null;
+
+        return ProductoDTO.builder()
+                .id(p.getId())
+                .nombre(p.getNombre())
+                .categoria(p.getCategoria())
+                .precio(p.getPrecio())
+                .build();
+    }
+
+    // Mapeo de Venta a VentaDTO
+    public static VentaDTO toDTO(Venta v) {
+        if (v == null) return null;
+
+        var detalle = v.getDetalle().stream().map(det -> DetalleVentaDTO.builder()
+                .id(det.getProd().getId())
+                .nombreProd(det.getProd().getNombre())
+                .cantProd(det.getCantProd())
+                .precio(det.getPrecio())
+                .subtotal(det.getPrecio() * det.getCantProd())
+                .build()
+        ).collect(Collectors.toList());
+
+        var total = detalle.stream()
+                .map(DetalleVentaDTO::getSubtotal)
+                .reduce(0.0, Double::sum);
+
+        return VentaDTO.builder()
+                .id(v.getId())
+                .fecha(v.getFecha())
+                .idSucursal(v.getSucursal().getId())
+                .estado(v.getEstado())
+                .detalle(detalle)
+                .total(total)
+                .build();
+    }
+
+
+    // Mapeo de Sucursal a SucursalDTO
+    public static SucursalDTO toDTO(Sucursal s) {
+        if (s == null) return null;
+
+        return SucursalDTO.builder()
+                .id(s.getId())
+                .nombre(s.getNombre())
+                .direccion(s.getDireccion())
+                .build();
+    }
+
+}
