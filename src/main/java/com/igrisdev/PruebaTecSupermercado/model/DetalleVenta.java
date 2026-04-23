e64a08960281979465db2f6016b6a8a730f5594a
igrisdev <johanalvarezz237@gmail.com>
2026-04-23 10:11:24 -0500

    feat: models

--- src/main/java/com/igrisdev/PruebaTecSupermercado/model/DetalleVenta.java
+++ src/main/java/com/igrisdev/PruebaTecSupermercado/model/DetalleVenta.java
@@ -1,4 +1,30 @@
 package com.igrisdev.PruebaTecSupermercado.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetalleVenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Venta
+    @ManyToOne
+    private Venta venta;
+
+    // Producto
+    @ManyToOne
+    private Producto prod;
+    private Integer cantProd;
+    private Double precio;
 }
--- src/main/java/com/igrisdev/PruebaTecSupermercado/model/Venta.java
+++ src/main/java/com/igrisdev/PruebaTecSupermercado/model/Venta.java
@@ -1,4 +1,23 @@
 package com.igrisdev.PruebaTecSupermercado.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate fecha;
+    private String estado;
+    @ManyToOne
+    private  Sucursal sucursal;
 }
